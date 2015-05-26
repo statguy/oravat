9372465b32b170e2f8f1ca2575440f020bef5848
statguy <jvj@iki.fi>
2015-05-27 00:03:35 +0700

    ...

--- oravat_semidense.R
+++ oravat_semidense.R
@@ -1,8 +1,10 @@
 if (F) {
   library(devtools)
   install_github("statguy/SpaceTime")
+  install_github("statguy/oravat")
   reload(inst("SpaceTime"))
 }
+
 library(SpaceTime)
 
 data <- read.csv2("SquirrelData_without_weather.csv", fileEncoding="latin1")
@@ -10,6 +12,8 @@ columns <- c("Year","X","Y","Length","marmar1km","marmarPrevYear","marmar2YearsA
 completeIndex <- complete.cases(data[,columns])
 oravat <- data[completeIndex, c("scivultracks", columns)]
 
+# cutoff=5e3, maxEdge=c(3.7e4, 2e5), offset=c(1e4, 4e4), convex=0.04 # dense mesh
+# cutoff=1e5, maxEdge=c(2e5, 10e5), convex=0.12 # sparse mesh
 mesh <- NonConvexHullMesh$new(knots=oravat[,c("X","Y")], knotsScale=1e6)$construct(cutoff=1e4, maxEdge=c(5e4, 2e5), offset=c(1e4, 4e4), convex=0.05)
 mesh$getINLAMesh()$n
 mesh$plot()
